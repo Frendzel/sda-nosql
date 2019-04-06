b699e4441334fed89be4c356ef0bd38fbe4802dc
marcinerbel <marcinerbel90@gmail.com>
2019-04-06 12:45:49 +0200

    Workshops 2 Find methods

--- mongodb-connector/src/test/groovy/com/sda/PropertyLoaderTest.groovy
+++ mongodb-connector/src/test/groovy/com/sda/PropertyLoaderTest.groovy
@@ -4,14 +4,26 @@ import spock.lang.Specification
 
 //TODO fill the test
 class PropertyLoaderTest extends Specification {
-    def "test getUser"() {
+    def "Should return correct #value for #key"() {
         given:
-        PropertyLoader loader = new PropertyLoader()
-        loader.init()
+            PropertyLoader loader = new PropertyLoader()
+            loader.init()
         expect:
-        value == loader.properties.get(key)
+            value == loader.properties.get(key)
         where:
-        key  || value
-        "DB" || "test"
+            key        || value
+            "DB"       || "test"
+            "user"     || "test"
+            "password" || "test123"
+            "address"  || "127.0.0.1"
+    }
+    def "Should return correct value for getUser method"(){
+        given:
+            PropertyLoader loader = new PropertyLoader()
+            loader.init()
+        when:
+            def user = loader.getUser()
+        then:
+            user == "test"
     }
 }
--- mongodb-connector/src/test/java/com/sda/MongoConnectorTest.java
+++ mongodb-connector/src/test/java/com/sda/MongoConnectorTest.java
@@ -1,11 +1,14 @@
 package com.sda;
 
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.junit.Before;
 import org.junit.Test;
 
+import static com.mongodb.client.model.Filters.*;
 import static org.junit.Assert.assertEquals;
 
 public class MongoConnectorTest {
@@ -33,5 +36,108 @@ public class MongoConnectorTest {
         //then
         MongoCollection<Document> grades = db.getCollection("grades");
         assertEquals(grades.estimatedDocumentCount(), 804);
+    }
+
+    @Test
+    public void getSize() {
+        //given
+        MongoConnector mongoConnector = new MongoConnector();
+        //when
+        Long result = mongoConnector.getSize("grades");
+        //then
+        assertEquals(result.longValue(), 804);
+    }
+
+    @Test
+    public void showAllRecords() {
+        //given
+        MongoConnector mongoConnector = new MongoConnector();
+        //when
+        mongoConnector.showAllRecords("grades");
+        //no then 😞
+    }
+
+    //findStudentsWhereIdLt10
+    @Test
+    public void findStudentWhereIdLt10() {
+        //given
+        MongoConnector mongoConnector = new MongoConnector();
+        //when
+        FindIterable<Document> studentsWhereIdLt10 = mongoConnector.findStudentsWhereIdLt10();
+        //then
+        for (Document document : studentsWhereIdLt10) {
+            System.out.println(document);
+        }
+
+    }
+    // tylko prace domowe
+    // db.grades.find({ type: { $eq: "homework" }})
+
+    @Test
+    public void findWithSuitableClause() {
+        //given
+        MongoConnector mongoConnector = new MongoConnector();
+        Bson bson = eq("type", "homework");
+        //when
+        FindIterable<Document> result = mongoConnector.findStudentsWithClause(bson);
+        //then
+        for (Document document : result) {
+            System.out.println(document);
+        }
+    }
+
+    // nie quiz i nie exam
+    // > db.grades.find({ type: { $nin: ["quiz", "exam"] }})
+    @Test
+    public void findWithSuitableClause2() {
+        //given
+        MongoConnector mongoConnector = new MongoConnector();
+        Bson bson = nin("type", "quiz", "exam");
+        //when
+        FindIterable<Document> result = mongoConnector.findStudentsWithClause(bson);
+        //then
+        for (Document document : result) {
+            System.out.println(document);
+        }
+    }
+
+    // student_id <10 && type = homework
+    // db.grades.find({   $and: [{student_id: {$lt: 10}},{type: {$eq: "homework"}}]})
+    @Test
+    public void findWithSuitableClause3() {
+        //given
+        MongoConnector mongoConnector = new MongoConnector();
+        Bson bson = and(
+                lt("student_id", "10"),
+                eq("type", "homework")
+        );
+        //when
+        FindIterable<Document> result = mongoConnector.findStudentsWithClause(bson);
+        //then
+        for (Document document : result) {
+            System.out.println(document);
+        }
+    }
+
+    // student_id in 1,2,3 and type exam or quiz
+    // db.grades.find({ $and: [{student_id: {$in: [1,2,3]}},
+    // {$or: [{type: {$eq: "exam"}},{type: {$eq: "quiz" }}]}]})
+    @Test
+    public void findWithSuitableClause4() {
+        //given
+        MongoConnector mongoConnector = new MongoConnector();
+        Bson bson = and(
+                in("student_id", 1, 2, 3),
+                or(
+                        eq("type", "exam"),
+                        eq("type", "quiz")
+                )
+        );
+        //when
+        FindIterable<Document> result = mongoConnector.findStudentsWithClause(bson);
+        //then
+        for (Document document : result) {
+            System.out.println(document);
+        }
     }
 }
