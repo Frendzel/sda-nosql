fb4950b19eaf0f15e464f1724696a5f6426be2c8
marcinerbel <marcinerbel90@gmail.com>
2019-05-19 12:15:48 +0200

    Workshops 2 - Jav16

--- mongodb-connector/src/test/java/com/sda/GradesApiTest.java
+++ mongodb-connector/src/test/java/com/sda/GradesApiTest.java
@@ -2,21 +2,23 @@ package com.sda;
 
 import com.mongodb.client.FindIterable;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.bson.json.JsonWriterSettings;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static com.mongodb.client.model.Filters.gt;
+
 class GradesApiTest {
 
     private Logger logger = LoggerFactory.getLogger(GradesApiTest.class);
+    private GradesApi api = new GradesApi();
 
     @Test
     @DisplayName("Should use find method on grades collection and return all values")
     void find() {
-        //given
-        GradesApi api = new GradesApi();
         //when
         FindIterable<Document> documents = api.find();
         //then
@@ -27,12 +29,38 @@ class GradesApiTest {
                             .indent(true)
                             .build()));
         }
+
     }
-//    @Test
-//    void findBy(){
-//        //given
-//        GradesApi api = new GradesApi();
-//        //when
-//        FindIterable<Document> documents = api.
-//    }
+
+    @Test
+    @DisplayName("Should use find by method with homework param on grades collection and return all values")
+    void findBy() {
+        //when
+        FindIterable<Document> documents = api.findBy(new Document("type", "homework"));
+        //then
+        for (Document document : documents) {
+            logger.info(document.toJson(
+                    JsonWriterSettings
+                            .builder()
+                            .indent(true)
+                            .build()));
+        }
+    }
+
+    @Test
+    @DisplayName("TODO")
+    void findBy2() {
+        //when
+        Bson filter = new Document("student_id", new Document("$gt", 20));
+        Bson filter2 = gt("student_id", 20);
+        FindIterable<Document> documents = api.findBy(filter);
+        //then
+        for (Document document : documents) {
+            logger.info(document.toJson(
+                    JsonWriterSettings
+                            .builder()
+                            .indent(true)
+                            .build()));
+        }
+    }
 }
